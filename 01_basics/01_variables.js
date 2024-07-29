5449049c1e6ca8cab2375212bb7aa5aabf0296d0
vikram976 <vikramkumarsingh1432@gmail.com>
2024-07-29 08:48:52 +0000

    let, var cost and empty 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const emailId = "hgjk@gmail.com "
+const pinCode = 1456667
+var address = "idorestadium"
+let city = "jaipur"
+
+//address = "rameshchowk"
+
+console.table([emailId, pinCode, address, city])
+
+// const emaiid cannot be reasigned
+// var can be changed due to scope issue
+    
