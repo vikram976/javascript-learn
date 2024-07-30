0e3a1d49d19e3fbbf1ea163168855ab64ea13333
vikram976 <vikramkumarsingh1432@gmail.com>
2024-07-30 11:45:23 +0000

    stringmethod

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+let details = "personalinfo"
+let firstName = "jignesh"
+let lastName = "raathi"
+
+//console.log(details + firstName + lastName);
+//console.log(`thisis my ${details}my ${firstName}and my${lastName}`);
+ 
+const userHandel = new String("striver")
+console.log(userHandel.indexOf('r'));
