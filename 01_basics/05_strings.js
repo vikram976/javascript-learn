1f8ff9622663c40b8c8b3ed846a3dfbd067fdf71
vikram976 <vikramkumarsingh1432@gmail.com>
2024-07-31 09:37:41 +0000

    stringmethod

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,7 +3,17 @@ let firstName = "jignesh"
 let lastName = "raathi"
 
 //console.log(details + firstName + lastName);
-//console.log(`thisis my ${details}my ${firstName}and my${lastName}`);
+console.log(`thisis my ${details}my ${firstName}and my${lastName}`);
  
-const userHandel = new String("striver")
-console.log(userHandel.indexOf('r'));
+const userHandel = new String("striver is fool")
+const userId = new String ('owner')
+//console.log(`${userHandel} ${userId}`);
+//console.log (userHandel.endsWith('joke'))//result=false//
+//console.log (userHandel.indexOf('fool'))
+//console.log (userHandel.lastIndexOf('fool'))
+//console.log (userHandel.slice(4, 9))
+//console.log (userHandel.split('i'))    
+//console.log (userHandel.includes('is'))
+//console.log (userHandel.replace('is', 'was'))
+//console.log (userHandel.toUpperCase())
+
