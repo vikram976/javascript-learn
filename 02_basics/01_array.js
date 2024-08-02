89f63a3fd8e79269b7709eab28fb751f65146f56
vikram976 <vikramkumarsingh1432@gmail.com>
2024-08-02 09:24:49 +0000

    arraybasics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+//array
+const myArr = [1,2,3,4,5]
+console.log (myArr);
+
+ //myArr.push(8)---inject 8 in main array
+ //myArr.pop()--------delet last element in array
+// myArr.unshift(1)-------inject element at 0 index
+//myArr.shift()-----delete elemnt present at zero index
+ const newArr= (myArr.slice(1,3));
+ console.log(newArr);
+// console.log (myArr.includes(8));------it check that mention element present in array or not 
+//const freshArr = myArr.splice(1,3);------- manipuilate orignal array
+console.log(freshArr);
