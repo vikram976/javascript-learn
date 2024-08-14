351d7ac1aa18d060f63cbf3a9c682b606b7dfb0e
vikram976 <vikramkumarsingh1432@gmail.com>
2024-08-14 07:42:47 +0000

    if else

---
+++ 04_basics/01_if-else.js
@@ -0,0 +1,63 @@
+
+// if (2 == 2) {
+//     console.log("correct")
+// } else {
+//     console.log("incorrect")
+// }
+
+// let x = 19
+//  if (x => 20) {
+//      console.log("underage");
+     
+//  } else {
+//     console.log("overage");
+    
+//  }
+// let score = 87
+
+// if (score > 90) {
+//     console.log("A=") 
+// } else if (score > 80) {
+//     console.log("B")
+// }else if (score > 70){
+//     console.log("C")
+// }else if (score > 60){
+//     console.log("D")
+// }else {
+//     console.log("low grade")
+// }
+//  const salary = 30000000
+//   if (salary <= 5000) {
+//     console.log("you can afford play model")
+//     if (salary <= 6000){
+//         console.log("you can afford shuttle also")
+//     }
+//   } else if(salary <= 20000) {
+//     console.log("you can afford high-end racket") 
+//   }else if(salary <= 50000){
+//     console.log("you can be a member")
+//     if(salary <= 45000){
+//         console.log("you are equipped")
+//     }
+//   }else if(salary <= 1000000){
+//     console.log("you can afford court")
+//   }else{
+//     console.log("you can afford anything")
+//   }
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+
+
+
+
